c6d6006d316e354cdee2ac65e8d01d02a343fd60
Maria Rita Severino <mariarsseverino@gmail.com>
2022-10-31 05:57:24 -0300

    colors

---
+++ app/src/utils/colors.ts
@@ -0,0 +1,11 @@
+export default {
+    dark: {
+        text: "#fff",
+        gray: "#7A7A7E",
+        background: "#040316",
+        primary: "#1A1A24",
+        secundary: "#854DFF",
+        pink: "#EA4C89",
+        yellow: "#f7bb0e",
+    },
+};
